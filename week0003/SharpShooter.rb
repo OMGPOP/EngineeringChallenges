e2f70705b65a672cf60ee39b6a9a4e171dae62c3
Andres Hernandez <andres@omgpop.com>
2011-10-07 17:46:18 -0400

    Sharpshooter

--- week0003/SharpShooter.rb
+++ week0003/SharpShooter.rb
@@ -16,23 +16,27 @@ class SharpShooter
     turn @base_speed*@tank_multi
     turn_gun @base_speed*@gun_multi*@dir
     if !events['robot_scanned'].empty?
-      #@gun_multi = 0
       alter_gun events['robot_scanned'].first.first
       @tank_multi = @tank_multi*@dir
       @dir = -@dir
-      #@accel_multi = @accel_multi*0.9;
+      @accel_multi = -2*@accel_multi*0.9*@dir
+      @flip_count = 0;
     else
       @gun_multi = 4
-      @tank_multi = 1.3
+      @tank_multi = 0.8
       @accel_multi = 1.3
+      @flip_count = @flip_count+1;
+      if(@flip_count == 5)
+        @dir = -@dir
+      end
     end
     fire 0.3
     
   end
   
   def init_vars
-    @base_speed = 1
-    @gun_multi = 5.5
+    @base_speed = 0.9
+    @gun_multi = 4
     @tank_multi = 1.6
     @accel_multi = 1.6
     @dir = 1;
