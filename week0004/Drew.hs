ea0a0daa314b160a99fcaebeb1ca68ddef83a36b
Drew Cummins <drew@omgpop.com>
2012-01-25 12:05:02 -0500

    little cleanup

--- week0004/Drew.hs
+++ week0004/Drew.hs
@@ -6,6 +6,5 @@ numDblSquares n = length [x^2 | x <- [0..isqrt $ div n 2], (isqrt (n - x^2))^2 +
 main = do
   args <- getArgs
   file <- readFile $ head args
-  let (_:ls) = lines file 
-  mapM_ putStrLn $ map (\n -> show $ numDblSquares (read n::Int)) ls
+  mapM_ putStrLn $ map (\n -> show $ numDblSquares (read n::Int)) (tail $ lines file)
 
