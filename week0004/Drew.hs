5ce422a074ec302657763cd76ab3512dcd965134
Drew Cummins <drew@omgpop.com>
2012-01-25 11:54:40 -0500

    solvd lol

--- week0004/Drew.hs
+++ week0004/Drew.hs
@@ -1,13 +1,11 @@
 import System (getArgs)
 
+numDblSquares n = length [x^2 | x <- [0..isqrt $ div n 2], (isqrt (n - x^2))^2 + x^2 == n]
+  where isqrt = floor . sqrt . fromIntegral
+
 main = do
   args <- getArgs
   file <- readFile $ head args
   let (_:ls) = lines file 
-  let isqrt = floor . sqrt . fromIntegral
-  mapM_ putStrLn $ map (\n -> numDblSquares (read n))
-
-numDblSquares :: Int -> Int
-numDblSquares n = length [x^2 | x <- [0..isqrt $ div n 2], (isqrt (n - x^2))^2 + x^2 == n]
-  where isqrt = floor . sqrt . fromIntegral
+  mapM_ putStrLn $ map (\n -> show $ numDblSquares (read n::Int)) ls
 
