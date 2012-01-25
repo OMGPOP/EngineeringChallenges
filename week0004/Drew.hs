8d8719d4b76fff4b13be84a88893abccaded9a59
Drew Cummins <drew@omgpop.com>
2012-01-25 14:11:01 -0500

    tightening

--- week0004/Drew.hs
+++ week0004/Drew.hs
@@ -1,6 +1,6 @@
 main = do
   file <- readFile $ "input.txt"
-  mapM_ putStrLn $ map (\n -> show $ numDblSquares (read n::Int)) (tail $ lines file)
+  mapM_ (putStrLn . show . numDblSquares . read) (tail $ lines file)
 
 isqrt = floor . sqrt . fromIntegral
 numDblSquares n = length [x | x <- [0..isqrt $ div n 2], (isqrt (n - x^2))^2 + x^2 == n]
