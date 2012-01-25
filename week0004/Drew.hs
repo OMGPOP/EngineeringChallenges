b241c0a048e457f70ecfb79aaae223726863542c
Drew Cummins <drew@omgpop.com>
2012-01-25 14:02:54 -0500

    lawls

--- week0004/Drew.hs
+++ week0004/Drew.hs
@@ -1,10 +1,6 @@
-import System (getArgs)
-
-numDblSquares n = length [x^2 | x <- [0..isqrt $ div n 2], (isqrt (n - x^2))^2 + x^2 == n]
-  where isqrt = floor . sqrt . fromIntegral
-
 main = do
-  args <- getArgs
-  file <- readFile $ head args
+  file <- readFile $ "input.txt"
   mapM_ putStrLn $ map (\n -> show $ numDblSquares (read n::Int)) (tail $ lines file)
 
+isqrt = floor . sqrt . fromIntegral
+numDblSquares n = length [x | x <- [0..isqrt $ div n 2], (isqrt (n - x^2))^2 + x^2 == n]
