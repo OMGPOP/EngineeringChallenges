68fa708dc6a5e1e5d23a25a6e4953ccb9a17e48b
William Chen <chenium@gmail.com>
2012-06-07 11:54:50 -0400

    optimized operations

--- week0007/Will.cpp
+++ week0007/Will.cpp
@@ -7,15 +7,14 @@ using namespace std;
 
 void solve( unsigned long int n, long int k )
 {
-	long int totalSolutions = n * (n - 1) * 0.5;
+	long int totalSolutions = ( n * (n - 1) ) >> 1; // divide 2
 	long int opp = totalSolutions - k + 1;
 	
-	double c = 2 * opp;
-	double ac = sqrt(1 + 4 * c);
+	double c = opp << 3; // multiply 8
+	double ac = sqrt(1 + c);
 	
 	long int solA = ceil( (-1 + ac) * 0.5 );
-	
-	long int numSolutions = solA * ( solA + 1 ) * 0.5;
+	long int numSolutions = ( solA * ( solA + 1 ) ) >> 1; // divide 2
 	
 	long int firstDigit = n - solA;
 	long int secondDigit = (numSolutions - opp) + firstDigit + 1;
