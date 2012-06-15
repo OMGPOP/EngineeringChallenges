7e29e0ec3d9a596d0cd89f3f4b9d0db32aa89ce6
Drew Cummins <drew@omgpop.com>
2012-06-15 14:17:36 -0400

    derp

--- week0007/drew.rb
+++ week0007/drew.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
-IO.foreach(ARGV[0]).each do |line|
-  n, k = line.split(" ").map(&:to_i)
-  x = (n - (1 + Math.sqrt((1 - (n<<1))**2 - ((k-1) << 3)))/2.0).to_i
+IO.binread(ARGV[0]).split("\n").map{ |x| x.split(" ").map(&:to_i) }.each do |n, k|
+  x = (n - (1 + Math.sqrt((1 - (n<<1))**2 - ((k-1)<<3)))/2.0).to_i
   y = k - (x * (n - (x + 3)/2.0)).to_i
   puts "#{x + 1} #{y + 1}"
 end
